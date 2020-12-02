a05b607578ca5869909bcf4070cbb5ba10169784
Albert Krewinkel <albert@zeitkraut.de>
2020-12-02 15:03:11 +0100

    lualib_in_haskell: add "run" target, update to current lts

--- examples/lualib_in_haskell/main.lua
+++ examples/lualib_in_haskell/main.lua
@@ -1,6 +1,6 @@
 require "lualibhelper"
 
 hs_init()
-print(add_in_haskell(1, 2))
-print(add_in_haskell(-10, 20))
+print("add_in_haskell(1,2)", add_in_haskell(1, 2))
+print("add_in_haskell(-10, 20)", add_in_haskell(-10, 20))
 hs_exit()
